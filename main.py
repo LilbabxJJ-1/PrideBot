db4aa6ee05e08ed541b4d794f5198d7aec5eb4b8
Jade <jaylan.franklin09@gmail.com>
2023-01-29 11:24:37 -0600

    Added owner command

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ dbl_token = dbltoken  # set this to your bot's Top.gg token
 bot.topggpy = topgg.DBLClient(bot, dbl_token, autopost=True, post_shard_count=True)
 
 
-@bot.slash_command(name="BCount")
-@bot.is_owner
+@bot.slash_command(name="bcount")
+@bot.is_owner()
 async def count(ctx):
     ctx.respond(f"Bot is in {len(bot.guilds)} guilds!")
 
